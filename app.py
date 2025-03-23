9e1ad0b6fba185eb6185c70f436a20ce5bf76a32
PiyushAdy <piyushady125@gmail.com>
2025-03-23 21:45:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -238,4 +238,5 @@ def logout():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all() # Create the database tables if they don't exist
-    app.run(host="0.0.0.0",port="8080",debug=True)  # Run the app
+    # app.run(host="0.0.0.0",port="8080",debug=True)  # Run the app
+    app.run()
